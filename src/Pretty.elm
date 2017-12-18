26d5fce264dfe66d66223991e3f587dab30d6567
Rupert Smith <rupert@thesett.com>
2017-12-18 15:10:10 +0000

    Added some helper functions needed to make it more useful.

--- src/Pretty.elm
+++ src/Pretty.elm
@@ -1,4 +1,22 @@
-module Pretty exposing (Doc)
+module Pretty
+    exposing
+        ( Doc
+        , (|+)
+        , append
+        , char
+        , empty
+        , string
+        , line
+        , group
+        , surround
+        , softline
+        , join
+        , space
+        , indent
+        , parens
+        , braces
+        , pretty
+        )
 
 {-| Pretty printer.
 @docs Doc
@@ -14,12 +32,18 @@ type Doc
     | Text String
     | Line
     | Union Doc Doc
+    | Nesting (Int -> Doc)
+    | Column (Int -> Doc)
 
 
 type Normal
     = NNil
     | NText String Normal
     | NLine Int Normal
+
+
+
+-- ==== Document constructors
 
 
 empty : Doc
@@ -48,6 +72,11 @@ string =
     Text
 
 
+char : Char -> Doc
+char c =
+    Text <| String.fromChar c
+
+
 line : Doc
 line =
     Line
@@ -56,6 +85,85 @@ line =
 group : Doc -> Doc
 group doc =
     Union (flatten doc) doc
+
+
+column : (Int -> Doc) -> Doc
+column =
+    Column
+
+
+nesting : (Int -> Doc) -> Doc
+nesting =
+    Nesting
+
+
+
+-- ==== Document helper functions
+
+
+surround : Doc -> Doc -> Doc -> Doc
+surround left right doc =
+    left |+ doc |+ right
+
+
+softline : Doc
+softline =
+    group Line
+
+
+join : Doc -> List Doc -> Doc
+join sep docs =
+    List.intersperse sep docs
+        |> List.foldr (\doc -> \remainder -> doc |+ remainder) empty
+
+
+space : Doc
+space =
+    char ' '
+
+
+parens : Doc -> Doc
+parens doc =
+    surround (char '(') (char ')') doc
+
+
+braces : Doc -> Doc
+braces doc =
+    surround (char '{') (char '}') doc
+
+
+align : Doc -> Doc
+align doc =
+    column
+        (\currentColumn ->
+            nesting
+                (\indentLvl -> nest (currentColumn - indentLvl) doc)
+        )
+
+
+hang : Int -> Doc -> Doc
+hang spaces doc =
+    align (nest spaces doc)
+
+
+indent : Int -> Doc -> Doc
+indent spaces doc =
+    string (copy spaces " ")
+        |+ doc
+        |> hang spaces
+
+
+
+-- ==== Pretty printing
+
+
+pretty : Int -> Doc -> String
+pretty w doc =
+    layout (best w 0 doc)
+
+
+
+-- ==== Internals
 
 
 flatten : Doc -> Doc
@@ -70,8 +178,8 @@ flatten doc =
         Union doc doc2 ->
             flatten doc
 
-        Line text ->
-            NText " "
+        Line ->
+            Text " "
 
         x ->
             x
@@ -126,17 +234,23 @@ best w k x =
                     better w
                         k
                         (be w k <| ( i, doc ) :: ds)
-                        (be w k <| ( i, doc2 ) :: ds)
+                        (\() -> (be w k <| ( i, doc2 ) :: ds))
+
+                ( i, Nesting fn ) :: ds ->
+                    be w k <| ( i, fn i ) :: ds
+
+                ( i, Column fn ) :: ds ->
+                    be w k <| ( i, fn k ) :: ds
     in
         be w k [ ( 0, x ) ]
 
 
-better : Int -> Int -> Normal -> Normal -> Normal
-better w k doc doc2 =
+better : Int -> Int -> Normal -> (() -> Normal) -> Normal
+better w k doc doc2Fn =
     if fits (w - k) doc then
         doc
     else
-        doc2
+        doc2Fn ()
 
 
 fits : Int -> Normal -> Bool
@@ -153,8 +267,3 @@ fits w normal =
 
             NLine int normal ->
                 True
-
-
-pretty : Int -> Doc -> String
-pretty w doc =
-    layout (best w 0 doc)
